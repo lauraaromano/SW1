5db12831e32e6205573d66af2f5c1934c9469809
lauraaromano <161627881+lauraaromano@users.noreply.github.com>
2025-02-18 10:32:08 -0300

    Add files via upload

---
+++ aula02/exemplo01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Oi 2F, sou a Laura</p>
+    <?php
+    $nome = "Laura";
+        echo "<p>Oi 2F, sou a Laura</p>";
+        echo "<p>Oi 2F, sou a $nome </p>";
+        echo "<p>Oi 2F, sou a ".$nome."</p>";
+    ?>
+</body>
+</html>
