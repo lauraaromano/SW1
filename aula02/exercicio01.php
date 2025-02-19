f463ac3bb50f1a1c825bf7f13db21029b1839f04
lauraaromano <161627881+lauraaromano@users.noreply.github.com>
2025-02-19 16:41:31 -0300

    Add files via upload

---
+++ aula02/exercicio01.php
@@ -0,0 +1,15 @@
+<?php
+$nome = "Laura";
+$idade = 16;
+$din = 400;
+
+echo "Olá! Qual o seu nome? ";
+echo $nome;
+
+echo " Oi " . $nome . "! Quantos anos você tem?";
+echo $idade;
+echo " Então você tem " . $idade . "!";
+
+echo " Quanto você ganha " . $nome . "?";
+echo $din;
+?>
---
+++ aula02/exercicio03.php
@@ -0,0 +1,19 @@
+<?php
+    $a = 20;
+    $b = 10;
+
+    $soma = $a + $b;
+    echo "Adição: $soma <br>";
+
+    $sub = $a - $b;
+    echo "Subtração: $sub  <br>";
+
+    $div = $a / $b;
+    echo "Divisão: $div <br>";
+
+    $mult = $a*$b;
+    echo "Multiplicação: $mult <br>";
+
+    $resto = $a % $b;
+    echo "Resto: $resto <br>";
+?>
