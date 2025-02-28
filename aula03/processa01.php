95ceda33a92b9038abe52080d076cef4522e145c
lauraaromano <161627881+lauraaromano@users.noreply.github.com>
2025-02-28 16:55:19 -0300

    Add files via upload

---
+++ aula03/processa01.php
@@ -0,0 +1,5 @@
+<?php
+    $nome = $_GET['cxnome'];
+
+    echo "Bem-vindo, $nome";
+?>
---
+++ aula03/processa_ex01.php
@@ -0,0 +1,11 @@
+<?php
+
+    $nome = $_POST['cxnome'];
+    $msg = $_POST['cxmsg'];
+
+    echo "Bem-vindo ao site, $nome";
+
+    echo "Sua mensagem é: $msg";
+
+?>
+
